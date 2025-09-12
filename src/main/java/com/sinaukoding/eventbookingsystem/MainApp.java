570bf05eca447da62e4614412bce064555e369e1
dwiiipoetra2 <dwi.waluyoputranto@gmail.com>
2025-09-12 15:20:58 +0700

    config profile app

--- src/main/java/com/sinaukoding/eventbookingsystem/MainApp.java
+++ src/main/java/com/sinaukoding/eventbookingsystem/MainApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookingappApplication {
+public class MainApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookingappApplication.class, args);
+		SpringApplication.run(MainApp.class, args);
 	}
 
 }
