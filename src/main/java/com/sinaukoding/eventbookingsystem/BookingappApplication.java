81f2917c56c05f5cc42b209f4323ed84aef65c42
dwiiipoetra2 <dwi.waluyoputranto@gmail.com>
2025-09-12 15:08:53 +0700

    first commit

---
+++ src/main/java/com/sinaukoding/eventbookingsystem/BookingappApplication.java
@@ -0,0 +1,13 @@
+package com.sinaukoding.eventbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingappApplication.class, args);
+	}
+
+}
